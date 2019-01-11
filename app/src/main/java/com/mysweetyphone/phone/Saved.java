e59cb5688fa8fa924bb1d901f0e9101fcea9502f
Roman Samara <rsamarar13@gmail.com>
2019-01-12 00:38:30 +0300

    Saved Commit#3

--- app/src/main/java/com/mysweetyphone/phone/Saved.java
+++ app/src/main/java/com/mysweetyphone/phone/Saved.java
@@ -1,16 +1,30 @@
 package com.mysweetyphone.phone;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+import java.util.regex.Pattern;
+
+import static android.app.Activity.RESULT_FIRST_USER;
+import static android.app.Activity.RESULT_OK;
 
 public class Saved extends Fragment {
 
@@ -33,20 +47,68 @@ public class Saved extends Fragment {
         super.onActivityCreated(savedInstanceState);
         final LinearLayout mainLayout = getActivity().findViewById(R.id.mainLayoutSAVED);
         final TextView msg =  getActivity().findViewById(R.id.textFieldSAVED);
+
+        final Button chooseFileButton = getActivity().findViewById(R.id.chooseFileSAVED);
+        chooseFileButton.setOnClickListener(new View.OnClickListener() {    //выбор файла
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M && PermissionChecker.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1001);
+                }
+                new MaterialFilePicker()
+                        .withActivity(getActivity())
+                        .withRequestCode(1)
+                        .withFilter(Pattern.compile(".*\\.$")) // Filtering files and directories by file name using regexp
+                        .withFilterDirectories(true) // Set directories filterable (false by default)
+                        .withHiddenFiles(true) // Show hidden files and folders
+                        .start();
+            }
+        });
+
         final Button sendButton = getActivity().findViewById(R.id.sendButtonSAVED);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                TextView sendText = new TextView(getActivity());
-                sendText.setTextColor(Color.BLACK);
-                sendText.setTextSize(21);
-                sendText.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.saved_box));
-                sendText.setPadding(40,40,40,40);
-                sendText.setText(msg.getText());
-                msg.setText("");
-                mainLayout.addView(sendText);
+                if(msg.getText().length() != 0){
+                    TextView sendText = new TextView(getActivity());
+                    sendText.setTextColor(Color.BLACK);
+                    sendText.setTextSize(21);
+                    sendText.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.saved_box));
+                    sendText.setPadding(40,40,40,40);
+                    sendText.setText(msg.getText());
+                    msg.setText("");
+                    mainLayout.addView(sendText);
+                }
             }
         });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+
+            Toast toast = Toast.makeText(getContext(),
+                    filePath, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @SuppressLint("ShowToast")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case  1001: {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getActivity(), "Permission granted.", Toast.LENGTH_SHORT);
+                }
+                else{
+                    Toast.makeText(getActivity(), "Permission NOT granted.", Toast.LENGTH_SHORT);
+                }
+            }
+        }
     }
 }
