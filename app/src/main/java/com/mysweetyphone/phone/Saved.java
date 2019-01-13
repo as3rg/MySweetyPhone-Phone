ee299abcecca462743519fe0e88a159332bc978f
Unknown <spyteamalex@gmail.com>
2019-01-13 16:11:43 +0300

    Fix Commit#2

--- app/src/main/java/com/mysweetyphone/phone/Saved.java
+++ app/src/main/java/com/mysweetyphone/phone/Saved.java
@@ -1,34 +1,26 @@
 package com.mysweetyphone.phone;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
-import android.support.v4.content.PermissionChecker;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.ViewTreeObserver;
+import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
-import java.util.regex.Pattern;
 
-import static android.app.Activity.RESULT_FIRST_USER;
 import static android.app.Activity.RESULT_OK;
 
 public class Saved extends Fragment {
@@ -52,7 +44,7 @@ public class Saved extends Fragment {
         super.onActivityCreated(savedInstanceState);
         final LinearLayout mainLayout = getActivity().findViewById(R.id.mainLayoutSAVED);
         final TextView msg =  getActivity().findViewById(R.id.textFieldSAVED);
-        final Button sendButton = getActivity().findViewById(R.id.sendButtonSAVED);
+        final ImageButton sendButton = getActivity().findViewById(R.id.sendButtonSAVED);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
@@ -62,12 +54,13 @@ public class Saved extends Fragment {
                 sendText.setTextSize(21);
                 sendText.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.saved_box));
                 sendText.setPadding(40,40,40,40);
-                sendText.setText(msg.getText());
+                sendText.setText(msg.getText().toString());
                 msg.setText("");
                 mainLayout.addView(sendText);
             }
         });
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -87,10 +80,10 @@ public class Saved extends Fragment {
         switch (requestCode){
             case  1001: {
                 if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    Toast.makeText(getActivity(), "Permission granted.", Toast.LENGTH_SHORT);
+                    Toast.makeText(getActivity(), "Permission granted.", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    Toast.makeText(getActivity(), "Permission NOT granted.", Toast.LENGTH_SHORT);
+                    Toast.makeText(getActivity(), "Permission NOT granted.", Toast.LENGTH_SHORT).show();
                 }
             }
         }
