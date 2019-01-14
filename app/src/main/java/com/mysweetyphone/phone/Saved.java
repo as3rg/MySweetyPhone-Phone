8a444991a3566a9b1c16d7f42442668b5afcc558
Roman Samara <rsamarar13@gmail.com>
2019-01-14 20:16:15 +0300

    Fix Commit#3

--- app/src/main/java/com/mysweetyphone/phone/Saved.java
+++ app/src/main/java/com/mysweetyphone/phone/Saved.java
@@ -5,40 +5,43 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
 import android.support.v4.content.PermissionChecker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
-import java.util.regex.Pattern;
 
-import static android.app.Activity.RESULT_FIRST_USER;
+import java.util.Date;
+
 import static android.app.Activity.RESULT_OK;
 
 public class Saved extends Fragment {
 
-    //Button sendButton;
-
+    boolean isMsgWillBeHereDeleted = false;
+    private long timeMessage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && PermissionChecker.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1001);
+        }
     }
 
     @Override
@@ -50,21 +53,57 @@ public class Saved extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        final LinearLayout mainLayout = getActivity().findViewById(R.id.mainLayoutSAVED);
-        final TextView msg =  getActivity().findViewById(R.id.textFieldSAVED);
-        final Button sendButton = getActivity().findViewById(R.id.sendButtonSAVED);
+        final LinearLayout mainLayout = getActivity().findViewById(R.id.MainLayoutSAVED);
+        final TextView msg =  getActivity().findViewById(R.id.TextFieldSAVED);
+        final ImageButton sendButton = getActivity().findViewById(R.id.SendButtonSAVED);
+        final TextView msgWillBeHere = getActivity().findViewById(R.id.MessagesWillBeHereSAVED);
+        final ListView listMessages = getActivity().findViewById(R.id.ListMessagesSAVED);
+
+        final ImageButton chooseFileButton = getActivity().findViewById(R.id.ChooseFileSAVED);
+        chooseFileButton.setOnClickListener(new View.OnClickListener() {    //выбор файла
+            @Override
+            public void onClick(View v) {
+                new MaterialFilePicker()
+                        .withActivity(getActivity())
+                        .withRequestCode(1000)
+                        .withHiddenFiles(false) // Показывает скрытые файлы и папки
+                        .start();
+            }
+        });
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                TextView sendText = new TextView(getActivity());
-                sendText.setTextColor(Color.BLACK);
+                if(!isMsgWillBeHereDeleted){
+                    mainLayout.removeView(msgWillBeHere);
+                    isMsgWillBeHereDeleted = true;
+                }
+
+                final String[] messages = new String[]{"s", "dfe", "s", "dfe","s", "dfe","s", "dfe","s", "dfe","s", "dfe","s", "dfe","s", "dfe",};
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                        R.layout.list_messages_saved, messages);
+
+                listMessages.setAdapter(adapter);
+
+                /*TextView sendText = new TextView(getActivity());
                 sendText.setTextSize(21);
                 sendText.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.saved_box));
-                sendText.setPadding(40,40,40,40);
-                sendText.setText(msg.getText());
-                msg.setText("");
-                mainLayout.addView(sendText);
+                sendText.setPadding(40, 40, 40, 40);
+
+                if(msg.getText().length() != 0) {
+                    sendText.setTextColor(Color.BLACK);
+                    sendText.setText(msg.getText().toString());
+                    msg.setText("");
+                    mainLayout.addView(sendText);
+                }
+                else{
+                    sendText.setTextColor(Color.parseColor("#494949"));
+                    sendText.setTypeface(null, Typeface.ITALIC);
+                    sendText.setText("Пустое сообщение");
+                    msg.setText("");
+                    mainLayout.addView(sendText);
+                }*/
             }
         });
     }
@@ -75,9 +114,8 @@ public class Saved extends Fragment {
         if (requestCode == 1000 && resultCode == RESULT_OK) {
             String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
 
-            Toast toast = Toast.makeText(getContext(),
-                    filePath, Toast.LENGTH_SHORT);
-            toast.show();
+            Toast.makeText(getActivity(), filePath, Toast.LENGTH_SHORT).show();
+
         }
     }
 
@@ -87,10 +125,11 @@ public class Saved extends Fragment {
         switch (requestCode){
             case  1001: {
                 if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    Toast.makeText(getActivity(), "Permission granted.", Toast.LENGTH_SHORT);
+                    Toast.makeText(getActivity(), "Permission granted.", Toast.LENGTH_SHORT).show();
+
                 }
                 else{
-                    Toast.makeText(getActivity(), "Permission NOT granted.", Toast.LENGTH_SHORT);
+                    Toast.makeText(getActivity(), "Permission NOT granted.", Toast.LENGTH_SHORT).show();
                 }
             }
         }
