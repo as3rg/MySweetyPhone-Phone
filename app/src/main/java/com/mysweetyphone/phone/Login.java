b8c27b40f0da825fe6e1b296a10cc5bb14635f12
Unknown <spyteamalex@gmail.com>
2019-03-20 23:20:56 +0300

    Starting Logo Commit#4

--- app/src/main/java/com/mysweetyphone/phone/Login.java
+++ app/src/main/java/com/mysweetyphone/phone/Login.java
@@ -44,6 +44,8 @@ public class Login extends AppCompatActivity {
                     }
                 }catch (Exception e){
                     e.printStackTrace();
+                    setTheme(R.style.AppTheme);
+                    setContentView(R.layout.activity_login);
                 }
             }
         });
